0a358dc4eb62197f5455626fb2a473c3c8055712
RTCWDF\chinnam <harish.chinnam@realtech.com>
2022-10-13 16:43:19 +0200

    Creating a Post submission using React app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+/*App.js */
+import React from 'react';
+import PostCreate from './PostCreate';
+
+const App = () => {
+    return(
+    <div className='container'>
+        <h1>Create Post</h1>
+        <PostCreate />
+    </div>);
+}
+
+export default App;
