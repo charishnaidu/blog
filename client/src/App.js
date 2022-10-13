b1f04c938bf653d648b8cfbba940989901dcf585
RTCWDF\chinnam <harish.chinnam@realtech.com>
2022-10-13 17:20:55 +0200

    Fetching and rendering posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 /*App.js */
 import React from 'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 const App = () => {
     return(
     <div className='container'>
         <h1>Create Post</h1>
         <PostCreate />
+        <hr />
+        <PostList />
     </div>);
 }
 
